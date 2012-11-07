588c0af4a3d845dcc2ad243a95d243d1bb39149b
Huyen Tran <hey.huyen@gmail.com>
2012-11-07 18:37:41 -0500

    minor changes for grid size generalization

--- life.py
+++ life.py
@@ -1,46 +1,63 @@
-SIZE = 3
-ORIGIN =[[0]*3, [1]*3, [0]*3]
+SIZE = 10
+BLOCK = [[0]*3, [0]+[1]*2, [0]+[1]*2]
+BLINKER = [[0]*3, [1]*3, [0]*3]
+TOAD = [[0]*4, [0]+[1]*3, [1]*3 +[0], [0]*4]
+BEACON = [[1]*2+[0]*2, [1]*2+[0]*2, [0]*2+[1]*2, [0]*2+[1]*2] 
+GLIDER = [  [0]*2+[1]+[0]*8,
+            [1]+[0]+[1]+[0]*8,
+            [0]+[1]*2+[0]*8,
+            [0]*11,
+            [0]*11,
+            [0]*11,
+            [0]*11,
+            [0]*11,
+            [0]*11,
+            [0]*11,
+            [0]*11 ]
 
-def print_universe(gen):
-    print '-' * 10
-    print gen[0]
-    print gen[1]
-    print gen[2]
+def print_universe(gen, universe):
+    print "Generation %d: " % gen
+    for row in universe:
+        print row[0:SIZE]
 
-def get_cell((row,col)):
-    if row in range(0,SIZE) and col in range(0,SIZE):
+def new_gen():
+    return [[0]*SIZE for i in range(0, SIZE)]
+
+def cell_state((row, col)):
+    if row in range(0, SIZE) and col in range(0, SIZE):
        return cur_gen[row][col]
     else:
         return 0
 
-def get_neighbors((row,col)):
+def get_neighbors((row, col)):
     x = [row - 1]*3 + [row]*2 + [row + 1]*3
     y = [col - 1, col, col + 1]*3
     y.pop(4) # remove middle index
-    neighbors = zip(x,y)
-    return [get_cell((row,col)) for row,col in neighbors]
+    return [cell_state((row, col)) for row, col in zip(x, y)]
 
-def next_life((row, col)):
+def next_state((row, col)):
     living = cur_gen[row][col]
-    neighbors = sum(get_neighbors((row, col)))
+    live_neighbors = sum(get_neighbors((row, col)))
     if living:
-        return neighbors in [2,3]
+        return int(live_neighbors in [2,3])
     else:
-        return neighbors == 3
-def new_gen():
-    return [[0]*3 for i in range(0,SIZE)]
+        return int(live_neighbors == 3)
 
 cur_gen = [[]]
-next_gen = new_gen() 
+next_gen = [[]] 
+
 if __name__ == "__main__":
-    cur_gen = ORIGIN
-
+    cur_gen = GLIDER
+    print "="*5 + "The Game of Life" + "="*5
+    gen = 0
     while True:
-        raw_input("GO")
         next_gen = new_gen()
-        print_universe(cur_gen) 
-
+        print_universe(gen, cur_gen) 
+        if raw_input("Hit ENTER to continue>"):
+            print "My life is over..."
+            break
         for i, row in enumerate(cur_gen):
             for j, cell in enumerate(row):
-                next_gen[i][j] = int(next_life((i, j)))
+                next_gen[i][j] = next_state((i, j))
         cur_gen = next_gen
+        gen += 1

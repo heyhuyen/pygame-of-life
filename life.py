48ca2388e8ade3a3aa69912ce7e8367d260a4306
Huyen Tran <hey.huyen@gmail.com>
2012-11-08 15:48:50 -0500

    created class Universe and put in universe.py leaving life.py to be the main program runner.

--- life.py
+++ life.py
@@ -1,63 +1,16 @@
-SIZE = 10
-BLOCK = [[0]*3, [0]+[1]*2, [0]+[1]*2]
-BLINKER = [[0]*3, [1]*3, [0]*3]
-TOAD = [[0]*4, [0]+[1]*3, [1]*3 +[0], [0]*4]
-BEACON = [[1]*2+[0]*2, [1]*2+[0]*2, [0]*2+[1]*2, [0]*2+[1]*2] 
-GLIDER = [  [0]*2+[1]+[0]*8,
-            [1]+[0]+[1]+[0]*8,
-            [0]+[1]*2+[0]*8,
-            [0]*11,
-            [0]*11,
-            [0]*11,
-            [0]*11,
-            [0]*11,
-            [0]*11,
-            [0]*11,
-            [0]*11 ]
-
-def print_universe(gen, universe):
-    print "Generation %d: " % gen
-    for row in universe:
-        print row[0:SIZE]
-
-def new_gen():
-    return [[0]*SIZE for i in range(0, SIZE)]
-
-def cell_state((row, col)):
-    if row in range(0, SIZE) and col in range(0, SIZE):
-       return cur_gen[row][col]
-    else:
-        return 0
-
-def get_neighbors((row, col)):
-    x = [row - 1]*3 + [row]*2 + [row + 1]*3
-    y = [col - 1, col, col + 1]*3
-    y.pop(4) # remove middle index
-    return [cell_state((row, col)) for row, col in zip(x, y)]
-
-def next_state((row, col)):
-    living = cur_gen[row][col]
-    live_neighbors = sum(get_neighbors((row, col)))
-    if living:
-        return int(live_neighbors in [2,3])
-    else:
-        return int(live_neighbors == 3)
-
-cur_gen = [[]]
-next_gen = [[]] 
+from universe import *
 
 if __name__ == "__main__":
-    cur_gen = GLIDER
+    uni = Universe(10, 10, GLIDER)
     print "="*5 + "The Game of Life" + "="*5
-    gen = 0
     while True:
-        next_gen = new_gen()
-        print_universe(gen, cur_gen) 
+        next_gen = new_gen(uni.height, uni.width)
+        uni.print_universe()
         if raw_input("Hit ENTER to continue>"):
             print "My life is over..."
             break
-        for i, row in enumerate(cur_gen):
+        for i, row in enumerate(uni.cells):
             for j, cell in enumerate(row):
-                next_gen[i][j] = next_state((i, j))
-        cur_gen = next_gen
-        gen += 1
+                next_gen[i][j] = uni.get_next_cell_state((i, j))
+        uni.cells = next_gen
+        uni.age += 1
---
+++ universe.py
@@ -0,0 +1,53 @@
+BLOCK = [[0]*3, [0]+[1]*2, [0]+[1]*2]
+BLINKER = [[0]*3, [1]*3, [0]*3]
+TOAD = [[0]*4, [0]+[1]*3, [1]*3 +[0], [0]*4]
+BEACON = [[1]*2+[0]*2, [1]*2+[0]*2, [0]*2+[1]*2, [0]*2+[1]*2] 
+GLIDER = [  [0]*2+[1]+[0]*7,
+            [1]+[0]+[1]+[0]*7,
+            [0]+[1]*2+[0]*7,
+            [0]*10,
+            [0]*10,
+            [0]*10,
+            [0]*10,
+            [0]*10,
+            [0]*10,
+            [0]*10 ]
+
+def new_gen(rows, cols):
+    return [[0]*cols for i in range(0, rows)]
+
+class Universe:
+
+    def __init__(self, height, width, origin=None):
+        self.width = height
+        self.height = width
+        self.age = 0
+        if not origin:
+            self.cells = new_gen(height, width)
+        else:
+            self.cells = origin
+
+    def print_universe(self):
+        print "Generation %d: " % self.age
+        for row in self.cells:
+            print row
+
+    def get_cell_state(self, (row, col)):
+        if row in range(0, self.height) and col in range(0, self.width):
+           return self.cells[row][col]
+        else:
+            return 0
+
+    def get_cell_neighbors(self, (row, col)):
+        x = [row - 1]*3 + [row]*2 + [row + 1]*3
+        y = [col - 1, col, col + 1]*3
+        y.pop(4) # remove middle index
+        return [self.get_cell_state((row, col)) for row, col in zip(x, y)]
+
+    def get_next_cell_state(self, (row, col)):
+        living = self.cells[row][col]
+        live_neighbors = sum(self.get_cell_neighbors((row, col)))
+        if living:
+            return int(live_neighbors in [2,3])
+        else:
+            return int(live_neighbors == 3)

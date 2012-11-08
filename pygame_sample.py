9ba2b50b8ebca185d43036eb3997dcb4e259b6c0
Huyen Tran <hey.huyen@gmail.com>
2012-11-08 14:29:31 -0500

    mouse events for hover, select and live cells working but can't deselect yet.

--- pygame_sample.py
+++ pygame_sample.py
@@ -2,18 +2,7 @@ import pdb
 import pygame
 import random
 import sys
-
-GRID_SIZE = 50
-CELL_SIZE = 10
-LINE_WIDTH = 1
-SIZE = GRID_SIZE * CELL_SIZE + LINE_WIDTH * (GRID_SIZE - 1)
-SCREEN_SIZE = (SIZE, SIZE)
-
-GREEN = (0, 255, 0)
-WHITE = (255, 255, 255)
-GRAY = (230, 230, 230)
-BLACK = (0, 0, 0)
-BG_COLOR = BLACK 
+from pygame.locals import *
 
 def draw_lines(surface):
     for x in range(1, GRID_SIZE):
@@ -21,29 +10,76 @@ def draw_lines(surface):
         pygame.draw.line(surface, GRAY, (pos, 0), (pos, SIZE), LINE_WIDTH)
         pygame.draw.line(surface, GRAY, (0, pos), (SIZE, pos), LINE_WIDTH)
 
-def draw_live_cell(surface, (x,y)):
-    x_pos = x*(CELL_SIZE + LINE_WIDTH)
-    y_pos = y*(CELL_SIZE + LINE_WIDTH)
+def draw_cell(surface, color, (i, j)):
+    # invert coordinates for drawing
+    x_pos = j * (CELL_SIZE + LINE_WIDTH)
+    y_pos = i * (CELL_SIZE + LINE_WIDTH)
     coords = (x_pos, y_pos, CELL_SIZE, CELL_SIZE)
-    #live_list.append(pygame.draw.rect(surface, GREEN, coords))
-    pygame.draw.rect(surface, GREEN, coords)
+    pygame.draw.rect(surface, color, coords)
 
-#live_list = []
+def draw_cells(surface, rects):
+    for rect in rects:
+        draw_cell(surface, rect[0], rect[1])
+
+def get_cell_indices((x,y)):
+    if x in range(1, SIZE-1) and y in range(1, SIZE-1):
+        cellnline = (CELL_SIZE + LINE_WIDTH)
+        i = y / cellnline
+        j = x / cellnline
+        if y % cellnline == 0 and x % cellnline == 0:
+            return None
+        return (i,j)
+    return None
+
+# drawing sizes
+GRID_SIZE = 4
+CELL_SIZE = 50
+LINE_WIDTH = 1
+SIZE = GRID_SIZE * CELL_SIZE + LINE_WIDTH * (GRID_SIZE - 1)
+SCREEN_SIZE = (SIZE, SIZE)
+
+# drawing colors
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+GRAY = (230, 230, 230)
+BLACK = (0, 0, 0)
+BG_COLOR = BLACK 
+
+# cell state colors
+ALIVE = GREEN
+HOVER = (0, 0, 255)
+SELECT = (255, 0, 0)
+
+# use (x, y) for drawing coordinates
+# use (i, j) for grid cell indices
 
 if __name__ == "__main__": 
     pygame.init()
+    mainclock = pygame.time.Clock()
     screen = pygame.display.set_mode(SCREEN_SIZE)
     pygame.display.set_caption('The Game of LIFE')
-    screen.fill(BG_COLOR)
-    draw_lines(screen)
-    pygame.display.update()
+    live_cells = []
     while True:
+        rect_list = []
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        #if len(live_list) > 0:
-        #    live_list[-1].
-        draw_live_cell(screen, (random.choice(range(0,GRID_SIZE)), random.choice(range(0, GRID_SIZE))))
+            elif event.type == MOUSEMOTION:
+                cell = get_cell_indices(event.pos)
+                if cell:
+                    rect_list.append((HOVER, cell))
+            elif event.type == MOUSEBUTTONDOWN:
+                cell = get_cell_indices(event.pos)
+                if cell:
+                    rect_list.append((SELECT, cell))
+            elif event.type == MOUSEBUTTONUP:
+                cell = get_cell_indices(event.pos)
+                if cell:
+                    live_cells.append((ALIVE, cell))
+        screen.fill(BG_COLOR)
+        draw_lines(screen)
+        draw_cells(screen, rect_list)
+        draw_cells(screen, live_cells)
         pygame.display.update()
-        #live_list.pop()
+        mainclock.tick(10)
